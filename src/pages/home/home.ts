0e1d46da6c87cc1abb555cbcb2e56c517a0ceb90
Tsy-Jon Lau <tsy-jon.lau@epitech.eu>
2017-10-28 15:35:12 +0200

    from scratch

---
+++ src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  googleSignin() {
+     firebase.auth().signInWithRedirect(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        console.log(token)
+        console.log(user)
+
+        //this.navCtrl.push(MainPage);
+
+     }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error.code)
+        console.log(error.message)
+     });
+  }
+
+}
