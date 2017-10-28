6f2b0a8d9b2191ae3a5058f83d23e38a313eff4d
DESKTOP-531B3R1\benjamin <benjamin.lepine@epitech.eu>
2017-10-29 01:34:21 +0200

    update

--- src/pages/list/list.module.ts
+++ src/pages/list/list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ListPage } from './signup';
+import { ListPage } from '../signup/signup';
 
 @NgModule({
   declarations: [
