cdd9b2f640fb9155b9a5b5a62c84aa7a3e0e89e9
Tsy-Jon Lau <tsy-jon.lau@epitech.eu>
2017-10-28 16:55:26 +0200

    firebase pb corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAmORyTzdXawXfm39yUDg8XoxLTeW-lj-8",
+  authDomain: "picday-39afd.firebaseapp.com",
+  databaseURL: "https://picday-39afd.firebaseio.com",
+  projectId: "picday-39afd",
+  storageBucket: "picday-39afd.appspot.com",
+  messagingSenderId: "146132231289"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -11,6 +23,8 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp(config);
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +33,3 @@ export class MyApp {
     });
   }
 }
-
