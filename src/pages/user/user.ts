4d978e1c8c9aff12da7eda2fb9ace4c93e6d854a
Tsy-Jon Lau <tsy-jon.lau@epitech.eu>
2017-11-02 16:20:39 +0100

    update user

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
-/**
- * Generated class for the UserPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  consultedUser: any;
+  consultedUid: any;
+  following: any;
+  currentUser: any;
+  images = [];
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public toastCtrl: ToastController) {
+    this.consultedUser = this.navParams.get("user");
+    this.consultedUid = this.navParams.get("uid");
+    this.following = this.navParams.get("following");
+    this.currentUser = firebase.auth().currentUser;
+    this.fetchGallery();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserPage');
+  fetchGallery() {
+    firebase.database().ref('/users/' + this.consultedUid + '/gallery').once('value')
+    .then(
+      (snapshot) => {
+        let value = snapshot.val();
+        for (let key in value) {
+          this.images.push(value[key]);
+        }
+      },
+      (error) => this.errorHandling(error)
+    );
+  }
+
+  addImageToGallery(image) {
+    let user = firebase.auth().currentUser;
+    firebase.database().ref().child('users/' + this.currentUser.uid + '/gallery/').push(image);
+  }
+
+  followUser() {
+    firebase.database().ref().child('users/' + this.currentUser.uid + '/following/').set({
+        [this.consultedUid]: this.consultedUser
+    });
+    this.following = true;
+  }
+
+  unfollowUser() {
+    firebase.database().ref().child('users/' + this.currentUser.uid + '/following/' + this.consultedUid).remove();
+    this.following = false;
+  }
+
+  errorHandling(error) {
+    let toast = this.toastCtrl.create({
+      message: "Error " + error.code + ": " + error.message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
